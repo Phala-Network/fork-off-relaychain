3098aba51ed47cb49d0a3b53b48893577c06e665
h4x3rotab <h4x3rotab@gmail.com>
2021-06-02 20:03:30 +0000

    Helper scripts

---
+++ fork.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+QUICK_MODE=1 HTTP_RPC_ENDPOINT=http://localhost:9933 ORIG_CHAIN=kusama FORK_CHAIN=kusama-local npm start
---
+++ node.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./data/binary --chain ./data/fork.json "$@"
